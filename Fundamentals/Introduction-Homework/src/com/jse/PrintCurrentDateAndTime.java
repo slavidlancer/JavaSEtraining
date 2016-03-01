3acb8db1f00ed391500f28c79ef87271a9a42609
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-01 21:26:23 +0200

    add PrintCurrentDateAndTime

---
+++ Fundamentals/Introduction-Homework/src/com/jse/PrintCurrentDateAndTime.java
@@ -0,0 +1,21 @@
+package com.jse;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class PrintCurrentDateAndTime {
+    public static void main(String[] args) {
+        LocalDateTime today = LocalDateTime.now(ZoneId.of("Europe/Sofia"));
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "dd.MM.yyyy г.\nHH:mm:ss ч.");
+        Date now = new Date();
+        
+        System.out.printf("%s %s %s, %s\n%s:%s h\n\n",today.getDayOfMonth(),
+                today.getMonth().toString().toLowerCase(), today.getYear(),
+                today.getDayOfWeek().toString().toLowerCase(), today.getHour(),
+                today.getMinute());
+        System.out.println(dateFormat.format(now));
+    }
+}

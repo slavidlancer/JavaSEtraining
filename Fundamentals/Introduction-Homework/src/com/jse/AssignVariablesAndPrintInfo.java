2cb83351567dc5db2c04db0b2ac0e7098589b6d3
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-01 20:51:27 +0200

    add Fundamentals set and first homework project AssignVariablesAndPrintInfo

---
+++ Fundamentals/Introduction-Homework/src/com/jse/AssignVariablesAndPrintInfo.java
@@ -0,0 +1,21 @@
+package com.jse;
+
+public class AssignVariablesAndPrintInfo {
+    public static void main(String[] args) {
+        byte byteValue = 127;
+        short shortValue = 32767;
+        int intValue = 32768;
+        int intValueOther = 2000000000;
+        long longValue = 919827112351L;
+        char charValue = 'c';
+        boolean booleanValue = false;
+        float floatValue = 0.5f;
+        double doubleValue = 0.1234567891011d;
+        String stringValue = "the string";
+        
+        System.out.println(byteValue + " " + shortValue + " " + intValue + " "
+                + intValueOther + " " + longValue + " " + charValue + " " +
+                booleanValue + " " + floatValue + " " + doubleValue + " " +
+                stringValue);
+    }
+}

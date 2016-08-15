577091cce2bce20555e629a27d0b29240825a30d
slavidlancer <slavidlancer@users.noreply.github.com>
2016-08-15 18:38:26 +0300

    complete ObtainTFromS from Codility test

--- Codility/src/com/jse/codility/test/ObtainTFromS.java
+++ Codility/src/com/jse/codility/test/ObtainTFromS.java
@@ -5,6 +5,9 @@ public class ObtainTFromS {
         String S = "nice";
         String T = "niece";
         
+        /*String S = "pout";
+        String T = "put";*/
+        
         /*String S = "from";
         String T = "form";*/
         
@@ -20,40 +23,148 @@ public class ObtainTFromS {
     }
     
     public String estimateHowToObtainTFromS(String S, String T) {
-        int sizeS = S.length();
-        int sizeT = T.length();
         String result = "IMPOSSIBLE";
+        String check = null;
+        
+        check = checkIfInsertionMakesStringsEqual(S, T);
+        
+        if (check != null) {
+            result = "INSERT " + check;
+        }
+        
+        check = checkIfDeletionMakesStringsEqual(S, T);
+        
+        if (check != null) {
+            result = "DELETE " + check;
+        }
+        
+        check = checkIfSwappingMakesStringsEqual(S, T);
+        
+        if (check != null) {
+            result = "SWAP " + check;
+        }
         
         if (S.equals(T)) {
             result = "NOTHING";
         }
         
-        /*for (int i = 0; i < Math.max(sizeS, sizeT); i++) {
-            for (int j = 0; j < Math.min(sizeS, sizeT); j++) {
-                if (S.charAt(i) == T.charAt(j)) {
-                    
-                }
-            }
-        }*/
-        
         return result;
     }
     
-    /* Given a string S and and String T, return:
-
-    "INSERT x" if string S can be obtained from String T by at most one insertion of character x.
-
-    For example, when S = "nice" and T = "niece", the method would return "INSERT e"
-
-    "DELETE x" if string T can be obtained from String S by at most one deletion of character x.
-
-    For example, when S = "hello" and T = "hell", the method would return "DELETE o"
-
-    "SWAP x z" if string T can be obtained from String S by swapping two adjacent characters.
-
-    For example, when S = "from" and T = "form", the method would return "SWAP r o"
-
-    "IMPOSSIBLE" if string T cannot be obtained from string S according to the rules above
-
-    "NOTHING" if string T equals string S */
+    private String checkIfInsertionMakesStringsEqual(String S, String T) {
+        int sizeS = S.length();
+        int sizeT = T.length();
+        
+        if ((sizeS + 1) != sizeT) {
+            return null;
+        }
+        
+        char[] sCharArr = S.toCharArray();
+        char[] tCharArr = T.toCharArray();
+        int sizeSArr = sCharArr.length;
+        int sizeTArr = tCharArr.length;
+        
+        for (int i = 0; i < sizeTArr; i++) {
+            if (i == (sizeTArr - 1)) {
+                String checkS = S + tCharArr[i];
+                
+                if (T.equals(checkS)) {
+                    return String.valueOf(tCharArr[i]);
+                } else {
+                    return null;
+                }
+            }
+            
+            if (sCharArr[i] != tCharArr[i]) {
+                String checkS = S.substring(0, i) + tCharArr[i] +
+                        S.substring(i, sizeSArr);
+                
+                if (T.equals(checkS)) {
+                    return String.valueOf(tCharArr[i]);
+                } else {
+                    return null;
+                }
+            } else {
+                continue;
+            }
+        }
+        
+        return null;
+    }
+    
+    private String checkIfDeletionMakesStringsEqual(String S, String T) {
+        int sizeS = S.length();
+        int sizeT = T.length();
+        
+        if ((sizeS - 1) != sizeT) {
+            return null;
+        }
+        
+        char[] sCharArr = S.toCharArray();
+        char[] tCharArr = T.toCharArray();
+        int sizeSArr = sCharArr.length;
+        //int sizeTArr = tCharArr.length;
+        
+        for (int i = 0; i < sizeSArr; i++) {
+            if (i == (sizeSArr - 1)) {
+                String checkS = S.substring(0, (sizeSArr -1));
+                
+                if (T.equals(checkS)) {
+                    return String.valueOf(sCharArr[i]);
+                } else {
+                    return null;
+                }
+            }
+            
+            if (sCharArr[i] != tCharArr[i]) {
+                String checkS = S.substring(0, i) + S.substring((i + 1),
+                        sizeSArr);
+                
+                if (T.equals(checkS)) {
+                    return String.valueOf(sCharArr[i]);
+                } else {
+                    return null;
+                }
+            } else {
+                continue;
+            }
+        }
+        
+        return null;
+    }
+    
+    private String checkIfSwappingMakesStringsEqual(String S, String T) {
+        int sizeS = S.length();
+        int sizeT = T.length();
+        
+        if (sizeS != sizeT) {
+            return null;
+        }
+        
+        char[] sCharArr = S.toCharArray();
+        //char[] tCharArr = T.toCharArray();
+        int sizeSArr = sCharArr.length;
+        //int sizeTArr = tCharArr.length;
+        
+        for (int i = 0; i < (sizeSArr - 1); i++) {
+            swapChars(sCharArr, i, (i + 1));
+            String checkS = new String(sCharArr);
+            
+            if (T.equals(checkS)) {
+                String swaps = sCharArr[i + 1] + " " + sCharArr[i];
+                
+                return swaps;
+            }
+            
+            swapChars(sCharArr, i, (i + 1));
+        }
+        
+        return null;
+    }
+    
+    private void swapChars(char[] charArr, int i, int j) {
+        char temp = charArr[i];
+        charArr[i] = charArr[j];
+        charArr[j] = temp;
+    }
 }
